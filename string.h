42fb44f14f77853a1418ba3f24a8fc83ac8504e2
Vishal_Kumar_Sharma <vishalsharma16171718@gmail.com>
2021-01-29 17:45:30 +0530

    added null character at the end of the string

--- string.h
+++ string.h
@@ -14,8 +14,9 @@ public:
 	String(const char *string)
 	{
 		m_Size = strlen(string);
-		m_Buffer = new char[m_Size];
+		m_Buffer = new char[m_Size+1];
 		memcpy(m_Buffer, string, m_Size);
+		m_Buffer[m_Size] = '\0';
 	}
 
 	friend std::ostream &operator<<(std::ostream &stream, const String &string);
