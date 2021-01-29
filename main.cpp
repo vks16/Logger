554832cfb009bbbb57a0a730baaaff0b0007e590
Vishal_Kumar_Sharma <vishalsharma16171718@gmail.com>
2021-01-29 14:04:56 +0530

    comments

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 
 int main(){
-    Log logger;
-    logger.SetLogLevel(logger.LOG_ERROR);
+    Log logger; // logger object
+    logger.SetLogLevel(logger.LOG_ERROR);  // setting log level to only display error messages
 
-    logger.info("THis is info");
-    logger.warn("This is warning");
-    logger.error("This is error");
+    logger.info("THis is info");   // this will not print because it is not error
+    logger.warn("This is warning"); // this will not print because it is not error
+    logger.error("This is error"); // this will print because it is error
 
     return 0;
 }
