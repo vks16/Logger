0a0227d28502ec5c76c3d765dfa21c61fd2bb06b
Vishal_Kumar_Sharma <vishalsharma16171718@gmail.com>
2021-01-29 15:59:54 +0530

    make methods const

--- Log.cpp
+++ Log.cpp
@@ -1,23 +1,24 @@
 #include "Log.h"
 
-void Log::SetLogLevel(int level){
-        m_LogLevel = level;
+void Log::SetLogLevel(int level)
+{
+    m_LogLevel = level;
 }
 
-void Log::info(const char* msg){
-        if(m_LogLevel >= LOG_INFO)
-            std::cout << "[Info]: " << msg << std::endl;
+void Log::info(const char *msg) const // const means this method is not going to modify any data inside this class
+{
+    if (m_LogLevel >= LOG_INFO)
+        std::cout << "[Info]: " << msg << std::endl;
 }
 
-void Log::warn(const char* msg)
-    {
-        if(m_LogLevel >= LOG_WARN)
-            std::cout << "[Warning]: " << msg << std::endl;
-    }
+void Log::warn(const char *msg) const
+{
+    if (m_LogLevel >= LOG_WARN)
+        std::cout << "[Warning]: " << msg << std::endl;
+}
 
-void Log::error(const char* msg)
-    {
-        if(m_LogLevel >= LOG_ERROR)
-            std::cout << "[Error]: " << msg << std::endl;
-    }
-
+void Log::error(const char *msg) const
+{
+    if (m_LogLevel >= LOG_ERROR)
+        std::cout << "[Error]: " << msg << std::endl;
+}
--- Log.h
+++ Log.h
@@ -2,26 +2,24 @@
 #define Log_H
 
 #include <iostream>
-class Log 
+class Log
 {
 public:
     const int LOG_INFO = 2;
     const int LOG_WARN = 1;
     const int LOG_ERROR = 0;
 
+private:
     int m_LogLevel = 0;
 
 public:
     void SetLogLevel(int level); // sets a log level
 
-    void info(const char* msg); // prints info message to console 
-   
+    void info(const char *msg) const; // prints info message to console
 
-    void warn(const char* msg); // prints warn message to console 
-  
+    void warn(const char *msg) const; // prints warn message to console
 
-    void error(const char* msg); // prints error message to console 
-    
+    void error(const char *msg) const; // prints error message to console
 };
 
 #endif // LOG_H
